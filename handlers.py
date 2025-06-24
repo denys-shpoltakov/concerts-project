b5cd97e065de3a84a437f52851be7375736ed26f
denys-shpoltakov <denysshpoltakov@gmail.com>
2025-06-24 17:06:48 +0200

    Added reply_markup to cmd '/start'

--- handlers.py
+++ handlers.py
@@ -2,10 +2,12 @@ from aiogram import Dispatcher, types, Bot, executor
 import texts_ru, texts_en
 from texts_ru import GREETINGS, HELP_CMD
 from dispatcher import dp
+from keyboards import start_kb
 
 @dp.message_handler(commands=['start'])
 async def start_cmd(message: types.Message):
-    await message.reply(GREETINGS)
+    await message.reply(GREETINGS,
+                        reply_markup=start_kb)
 
 @dp.message_handler(commands=['help'])
 async def start_cmd(message: types.Message):
