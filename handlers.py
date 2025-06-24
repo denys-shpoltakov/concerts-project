b51ac0bf59579d5062d79fb53fda005941013adf
denys-shpoltakov <denysshpoltakov@gmail.com>
2025-06-24 17:01:54 +0200

    Added new variable HELP_CMD and added new cmd /help

--- handlers.py
+++ handlers.py
@@ -1,8 +1,12 @@
 from aiogram import Dispatcher, types, Bot, executor
 import texts_ru, texts_en
-from texts_ru import GREETINGS
+from texts_ru import GREETINGS, HELP_CMD
 from dispatcher import dp
 
 @dp.message_handler(commands=['start'])
 async def start_cmd(message: types.Message):
     await message.reply(GREETINGS)
+
+@dp.message_handler(commands=['help'])
+async def start_cmd(message: types.Message):
+    await message.reply(HELP_CMD)
