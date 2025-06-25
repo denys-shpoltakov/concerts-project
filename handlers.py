dd34836131205bf78137b363d0eb456131145979
denys-shpoltakov <denysshpoltakov@gmail.com>
2025-06-25 14:02:14 +0200

    added the ability to write a command not with /help, but with just the word ‘Help’.

--- handlers.py
+++ handlers.py
@@ -1,6 +1,7 @@
 from aiogram import Dispatcher, types, Bot, executor
 import texts_ru, texts_en
 from texts_ru import GREETINGS, HELP_CMD, DESC_CMD, EVENTS_CAPTION
+from aiogram.dispatcher.filters import Text
 from dispatcher import dp
 from keyboards import start_kb
 
@@ -8,6 +9,10 @@ from keyboards import start_kb
 async def start_cmd(message: types.Message):
     await message.reply(GREETINGS,
                         reply_markup=start_kb)
+
+@dp.message_handler(Text(equals='Помощь'))
+async def help_filter_cmd(message: types.Message):
+    await message.reply(HELP_CMD)
 
 @dp.message_handler(commands=['help'])
 async def help_cmd(message: types.Message):
--- keyboards.py
+++ keyboards.py
@@ -2,6 +2,6 @@ from aiogram import types
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 start_kb = ReplyKeyboardMarkup(resize_keyboard=True)
-help_btn = KeyboardButton('/help')
+help_btn = KeyboardButton('Помощь')
 
 start_kb.add(help_btn)
