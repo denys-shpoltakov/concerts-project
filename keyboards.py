3dcdbfb7eb01d00610b127587bdf3b4141902613
denys-shpoltakov <denysshpoltakov@gmail.com>
2025-06-25 14:51:15 +0200

    Fixed bugs with started-keyboard.

--- keyboards.py
+++ keyboards.py
@@ -2,11 +2,13 @@ from aiogram import types
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 start_kb = ReplyKeyboardMarkup(resize_keyboard=True)
+
 help_btn = KeyboardButton('Помощь')
 desc_btn = KeyboardButton('Описание бота')
 events_btn = KeyboardButton('Посмотреть мероприятия')
 
-start_kb.add(help_btn).insert(desc_btn, events_btn)
+start_kb.add(help_btn).insert(desc_btn)
+start_kb.insert(events_btn)
 
 #--------------------------------------#
 
