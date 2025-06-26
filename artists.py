a2315dd548e1b6517cf4ed383489b2c7ef0be6ed
denys-shpoltakov <denysshpoltakov@gmail.com>
2025-06-26 16:28:04 +0200

    Changed from photo to video in artists handler.

--- artists.py
+++ artists.py
@@ -8,8 +8,8 @@ from texts_ru import KIZARU_TEXT
 
 @dp.message_handler(Text(equals='kizaru'))
 async def kizaru_cmd(message: types.Message):
-    kizaru_photo = InputFile('photos/kizaru.jpg')
-    await message.bot.send_photo(chat_id=message.from_user.id,
+    kizaru_video = InputFile('videos/kizaru.mp4')
+    await message.bot.send_video(chat_id=message.from_user.id,
                                  caption=KIZARU_TEXT,
-                                 photo=kizaru_photo,
+                                 video=kizaru_video,
                                  reply_markup=events_kb)
