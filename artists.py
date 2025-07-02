ca4e5c6f22376a0b34ce36bebf1985dd9d36d3b4
denys-shpoltakov <denysshpoltakov@gmail.com>
2025-07-02 14:21:47 +0200

    Changed text in texts_ru.py and in artists dispatcher.

--- artists.py
+++ artists.py
@@ -3,7 +3,7 @@ from aiogram.dispatcher.filters import Text
 from keyboards import start_kb, events_kb, kizaru_buy_ticket_kb
 from dispatcher import dp
 from aiogram.types import InputFile
-from texts_ru import KIZARU_TEXT
+from texts_ru import KIZARU_TEXT, BBT_TEXT
 
 
 @dp.message_handler(Text(equals='kizaru'))
@@ -18,6 +18,6 @@ async def kizaru_cmd(message: types.Message):
 async def kizaru_cmd(message: types.Message):
     bbt_photo = InputFile('photos/bigbabytape.jpg')
     await message.bot.send_photo(chat_id=message.from_user.id,
-                                 caption=KIZARU_TEXT,
+                                 caption=BBT_TEXT,
                                  photo=bbt_photo,
                                  reply_markup=kizaru_buy_ticket_kb)
